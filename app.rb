6db4428d4afcc37b4f92f4639b5cff5f92dc0257
Jp1200 <jprkr96@gmail.com>
2020-02-19 11:52:56 -0600

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,14 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    org =  params["string"]
+    @rev_str = org.reverse
+
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
-
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
   end
 end
